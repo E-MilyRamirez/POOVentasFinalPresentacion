176d50e2549272d586d76a5e7968e0faa4df5732
ramir <emilyram006@gmail.com>
2025-04-01 07:38:45 -0700

    Diseño de FrmVentas

--- POOVentasFinal/FrmVenta.cs
+++ POOVentasFinal/FrmVenta.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace POOVentasFinal
+﻿namespace POOVentasFinal
 {
     public partial class FrmVenta : Form
     {
+        List<ConceptoVenta> conceptos;
         public FrmVenta()
         {
             InitializeComponent();
+            List<ConceptoVenta> conceptos = new List<ConceptoVenta>();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            DgvConceptos.DataSource = null;
+
+            ConceptoVenta concepto = new ConceptoVenta();
+            concepto.Cantidad = Convert.ToInt32(TxtCantidad.Text);
+            concepto.Descripcion = TxtDescripcion.Text;
+            concepto.ValorUnitario = Convert.ToDecimal(TxtValorUnitario.Text);
+            conceptos.Add(concepto);
+
+            DgvConceptos.DataSource = conceptos;
         }
     }
 }
